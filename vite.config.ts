5516226c965c945adadae61f3ab40803156e98ff
zengxingqi <me@zengxingqi.com>
2023-01-03 15:38:17 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite'
+import Inspect from 'vite-plugin-inspect'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "./src/assets/scss/common.scss" as *;',
+      },
+    },
+  },
   plugins: [
     vue(),
     eslintPlugin({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/**/*.jsx', 'src/**/*.ts'],
       exclude: ['./node_modules/**'],
       cache: false
-    })
+    }),
+    Inspect(),
   ]
 })
